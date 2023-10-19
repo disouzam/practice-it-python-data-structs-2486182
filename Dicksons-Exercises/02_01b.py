f7cbd23553cd431fac9df08d8751ae1955932690
Dickson Souza <disouzam@yahoo.com.br>
2023-10-18 22:26:35 -0300

    Instantiated deque and printed an empty deque

--- Dicksons-Exercises/02_01b.py
+++ Dicksons-Exercises/02_01b.py
@@ -1,6 +1,9 @@
+from collections import deque
 
 def main():
     #add code here
+    last_five_products_bought = deque(maxlen= 5)
+    print(last_five_products_bought)
     return
 
 if __name__ == "__main__":
